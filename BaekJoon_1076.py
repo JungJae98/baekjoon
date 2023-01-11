c269a9b8ffd4e28c2bb8b34dee6bfbb43a7c02c6
aabds <wogus8880@gmail.com>
2023-01-11 22:42:37 +0900

    저항

---
+++ BaekJoon_1076.py
@@ -0,0 +1,7 @@
+color = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+
+result1 = color.index(input())
+result2 = color.index(input())
+multiply = color.index(input())
+
+print(int(str(result1)+str(result2))*(10**multiply))
